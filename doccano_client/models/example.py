6fc7dd5f026384b03cc2c4d601a333756ace9902
Hironsan <hiroki.nakayama.py@gmail.com>
2022-10-11 09:39:55 +0900

    Update example model to accept image and audio files

--- doccano_client/models/example.py
+++ doccano_client/models/example.py
@@ -7,7 +7,7 @@ class Example(BaseModel):
     """Contains the data relevant to a example on a Doccano project"""
 
     id: Optional[int]
-    text: str = ""
+    text: Optional[str] = None
     meta: Dict[str, Any] = Field(default_factory=dict)
     annotation_approver: Optional[str] = None
     comment_count: int = 0
