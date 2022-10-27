a68fbfc0af788c3df4c92e9b6a0e31d8e29a49cc
Hironsan <light.tree.1.13@gmail.com>
2022-10-27 14:50:40 +0900

    Update test cases

--- tests/unit/models/test_user_details.py
+++ tests/unit/models/test_user_details.py
@@ -3,19 +3,21 @@ import pytest
 from doccano_client.models.user_details import PasswordChange
 
 
-class TestPasswordChange:
-    def test_correct_password(self):
-        password_change = PasswordChange(new_password="foobarbaz", confirm_password="foobarbaz")
-        assert password_change.new_password == password_change.confirm_password
+def test_correct_password():
+    password_change = PasswordChange(new_password="foobarbaz", confirm_password="foobarbaz")
+    assert password_change.new_password == password_change.confirm_password
 
-    def test_raise_error_with_short_password(self):
-        with pytest.raises(ValueError):
-            PasswordChange(new_password="f", confirm_password="f")
 
-    def test_raise_error_with_long_password(self):
-        with pytest.raises(ValueError):
-            PasswordChange(new_password="f" * 129, confirm_password="f" * 129)
+def test_raise_error_with_short_password():
+    with pytest.raises(ValueError):
+        PasswordChange(new_password="f", confirm_password="f")
 
-    def test_raise_error_if_new_password_does_not_match_confirm_password(self):
-        with pytest.raises(ValueError):
-            PasswordChange(new_password="foobarbaz", confirm_password="zabraboof")
+
+def test_raise_error_with_long_password():
+    with pytest.raises(ValueError):
+        PasswordChange(new_password="f" * 129, confirm_password="f" * 129)
+
+
+def test_raise_error_if_new_password_does_not_match_confirm_password():
+    with pytest.raises(ValueError):
+        PasswordChange(new_password="foobarbaz", confirm_password="zabraboof")
