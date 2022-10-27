b8193a7d81bba7597b108ab55fb90f7d3d40adc5
David Engelmann <81196676+david-engelmann@users.noreply.github.com>
2022-10-27 08:57:54 -0400

    Add create_user func
    Basic mechanics for creating a user through requests

--- doccano_client/repositories/user_details.py
+++ doccano_client/repositories/user_details.py
@@ -50,3 +50,17 @@ class UserDetailsRepository:
             json={"new_password1": password_change.new_password, "new_password2": password_change.confirm_password},
         )
         return PasswordUpdated.parse_obj(response.json())
+
+     def create_user(self, username: str, password: str):
+        """Create new user
+        Args:
+            username (str): the username of the user thats to be created
+            password (str): the password to set for the new user
+        Returns:
+            Not Sure Yet
+        """
+        response = self._client.post(
+            "v1/users/creation/",
+            json={"username": username, "password1": password, "password2": password},
+        )
+        return response.json()
