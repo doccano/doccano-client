7fe2642537110de18f7ceefdf590e9c1657ba70c
Hironsan <hiroki.nakayama.py@gmail.com>
2022-10-28 10:35:49 +0900

    Move DoccanoAPIError to exceptions.py

--- doccano_client/repositories/base.py
+++ doccano_client/repositories/base.py
@@ -5,15 +5,7 @@ from typing import Optional
 import requests
 from requests import Response, exceptions
 
-
-class DoccanoAPIError(exceptions.HTTPError):
-    def __init__(self, response: Response):
-        """Initialize the exception with the response.
-
-        Args:
-            response (Response): The response to initialize the exception
-        """
-        super().__init__(str(response.json()), response=response)
+from doccano_client.exceptions import DoccanoAPIError
 
 
 def verbose_raise_for_status(response: Response) -> Response:
